93fe6845794352cce769ffef7a0406e780b950e5
mikanchu <mikan1985@gmail.com>
2019-08-17 15:46:44 +0200

    add example code for 4step motor

--- README.md
+++ README.md
@@ -11,3 +11,23 @@
 * Protoboards
 * Displays
 * Sensor LM35
+
+# Cases
+
+## four step motor
+
+For the 4 step motor, we used `Step motor 28BYJ-48`.
+
+We connected it on the following ports.
+
+| Arduino port | Controller port|
+|:------------:|:--------------:|
+| Digital 9    | IN1            |
+| Digital 10   | IN2            |
+| Digital 11   | IN3            |
+| Digital 12   | IN4            |
+
+The controller accepts an input of 5 or 12V. We chose to use 5V and have it plugged directly to the board.
+
+The code can be found in the `arduinosrc`
+
---
+++ arduinosrc/four_step_motor/motor_test.ino
@@ -0,0 +1,96 @@
+#define STEPPER_PIN_1 9
+#define STEPPER_PIN_2 10
+#define STEPPER_PIN_3 11
+#define STEPPER_PIN_4 12
+int step_number = 0;
+int times = 0;
+int speed = 2; // Max speed. Bigger numbers, slower
+int stop_time = 2000;  // time to stop, in ms
+bool reverse = false;  // Direction of rotation
+
+void setup() {
+  pinMode(STEPPER_PIN_1, OUTPUT);
+  pinMode(STEPPER_PIN_2, OUTPUT);
+  pinMode(STEPPER_PIN_3, OUTPUT);
+  pinMode(STEPPER_PIN_4, OUTPUT);
+}
+
+void loop() {
+  // This function rotates the motor 180degrees, stops 2s goes reverse and stops again.
+  // 1024 is the number if steps for half a revolution
+  while (times < 1024){
+      OneStep(reverse);
+      times++;
+  }
+  stop();
+}
+
+void OneStep(bool direction){
+   if (direction){
+    switch(step_number){
+      case 0:
+        digitalWrite(STEPPER_PIN_1, HIGH);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      case 1:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, HIGH);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      case 2:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, HIGH);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      case 3:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, HIGH);
+        break;
+    }
+  } else {
+    switch(step_number){
+      case 0:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, HIGH);
+        break;
+      case 1:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, HIGH);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      case 2:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, HIGH);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      case 3:
+        digitalWrite(STEPPER_PIN_1, HIGH);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+    }
+  }
+  delay(speed);
+  step_number = (step_number + 1)%4;
+}
+
+void stop(){
+  digitalWrite(STEPPER_PIN_1, LOW);
+  digitalWrite(STEPPER_PIN_2, LOW);
+  digitalWrite(STEPPER_PIN_3, LOW);
+  digitalWrite(STEPPER_PIN_4, LOW);
+  times = 0;
+  reverse = !reverse;
+  delay(stop_time);
+}
